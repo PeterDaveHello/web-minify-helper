db6bc635dc9dcc0f043316867c36ff91948b3e4f
Peter Dave Hello <hsu@peterdavehello.org>
2016-08-26 17:44:22 +0800

    refactor

--- minify.sh
+++ minify.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-MYPATH=`dirname "$0"`
+MYPATH=$(dirname "$0")
 
-. $MYPATH/ColorEchoForShell/dist/ColorEcho.bash
+. "$MYPATH/ColorEchoForShell/dist/ColorEcho.bash"
 
 #ls time format depends on OS
-if [ "FreeBSD" = `uname` ] || [ "Darwin"  = `uname` ]; then
+if [ "FreeBSD" = "$(uname)" ] || [ "Darwin"  = "$(uname)" ]; then
     LS='ls -l -D %Y%m%d%H%M%S'
 else
     LS='ls -l --time-style +%Y%m%d%H%M%S'
@@ -19,7 +19,7 @@ else
         TARGET='.'
         NP=true
     else
-        TARGET=$1
+        TARGET="$1"
         if [ ! -z "$2" ] && [ "$2" = "--no-map" ]; then
             NP=true
         else
@@ -37,30 +37,30 @@ urls[css]="http://cssminifier.com/raw"
 echo.BoldCyan "Scaning direcotry..."
 
 #list all the directories except git directory
-for dir in `find $TARGET -type d | grep -v \/\.git\/`
+for dir in $(find "$TARGET" -type d | grep -E -v '\.git(\/|$)')
 do
-    if [ ! -w $dir ]; then
+    if [ ! -w "$dir" ]; then
         echo.BoldRed "$dir is not writable, ignore it."
         continue
     fi
-
-    cd $dir
+    (
+    cd "$dir" || exit
     for filetype in "${types[@]}"
     do
         echo.Green "Finding $filetype to be compressed under $dir ..."
         #list js/css files exclude already minified files
-        for filename in `ls *.$filetype 2> /dev/null | sed "s/\.$filetype$//g" | grep -v .min$`
+        for filename in $(ls *.$filetype 2> /dev/null | sed "s/\.$filetype$//g" | grep -v .min$)
         do
             do_min=0
-            #echek if exist a minified version
-            if [ -f $filename\.min\.$filetype ]; then
+            #check if exist a minified version
+            if [ -f "${filename}.min.$filetype" ]; then
                 #already exist a minified version, compare the modify time to decide compressing or not
-                orig_ver_time=`eval $LS $filename\.$filetype | awk '{print $6}'`
-                mini_ver_time=`eval $LS $filename\.min\.$filetype | awk '{print $6}'`
-                if [ $mini_ver_time -lt $orig_ver_time ]; then
+                orig_ver_time=$(eval "$LS" "${filename}.$filetype" | awk '{print $6}')
+                mini_ver_time=$(eval "$LS" "${filename}.min.$filetype" | awk '{print $6}')
+                if [ "$mini_ver_time" -lt "$orig_ver_time" ]; then
                     do_min=1
                 fi
-            elif [ `wc -l $filename.$filetype | awk '{print $1}'` -gt 8 ]; then
+            elif [ $(wc -l "$filename.$filetype" | awk '{print $1}') -gt 8 ]; then
                 do_min=1
             fi
             MAP_OP=""
@@ -70,21 +70,21 @@ do
                     if [ ! "$NP" = "true" ]; then
                         MAP_OP="--source-map"
                     fi
-                    $MYPATH/node_modules/clean-css/bin/cleancss --compatibility $MAP_OP --s0 -o $filename\.min.$filetype $filename.$filetype
+                    "$MYPATH/node_modules/clean-css/bin/cleancss" --compatibility $MAP_OP --s0 -o "${filename}.min.$filetype" "$filename.$filetype"
                 else
                     if [ ! "$NP" = "true" ]; then
-                        MAP_OP="--source-map $filename.min.$filetype.map"
+                        MAP_OP="--source-map ${filename}.min.$filetype.map"
                     fi
-                    $MYPATH/node_modules/uglify-js/bin/uglifyjs --mangle --compress if_return=true $MAP_OP -o $filename\.min.$filetype $filename.$filetype
+                    "$MYPATH/node_modules/uglify-js/bin/uglifyjs" --mangle --compress if_return=true $MAP_OP -o "${filename}.min.$filetype" "${filename}.$filetype"
                 fi
                 if [ ! $? -eq 0 ]; then
                     echo.Red "local compressor failed, now try to compress with javascript-/cssminifier.com"
-                    curl -X POST -s --data-urlencode "input@$filename.$filetype" ${urls[$filetype]} > $filename\.min.$filetype
+                    curl -X POST -s --data-urlencode "input@${filename}.$filetype" ${urls[$filetype]} > "${filename}.min.$filetype"
                 fi
             fi
         done
     done
-    cd - &> /dev/null
+    )
 done
 
 echo.BoldGreen "All done!"
